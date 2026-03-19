9d3c4a0a8334ece696f3e6b3b38e06a45948d894
janithsadanuwan1234 <shitharasubhashani@gmail.com>
2026-03-19 22:45:49 +0530

    s

--- plugins/alive.cjs
+++ plugins/alive.cjs
@@ -1,5 +1,5 @@
 const { cmd, commands } = require('../command.cjs');
-const config = require('../config.cjs.cjs');
+const config = require('../config.cjs');
 const aliveMsg = require('./aliveMsg.cjs');
 const axios = require('axios'); 
 
--- plugins/paid_users.cjs
+++ plugins/paid_users.cjs
@@ -1,5 +1,5 @@
 const { cmd } = require('../command.cjs');
-const { updateSetting } = require('../plugins/bot_db');
+const { updateSetting } = require('../plugins/bot_db.cjs');
 
 // 🛡️ අවසර ලත් අංක (Owner only)
 const allowedNumbers = ["94771810698", "94743404814", "94766247995", "192063001874499", "270819766866076"];
