f1fa8cc1531ab98ef218813378fd265314ae3397
Janiya_X <95123183+Janithsadanuwan@users.noreply.github.com>
2026-03-20 19:18:58 +0530

    s

--- plugins/alive.cjs
+++ plugins/alive.cjs
@@ -32,6 +32,44 @@ cmd({
 },
 async (zanta, mek, m, { from, reply,q, userSettings }) => {
     try {
+        const moment = require("moment-timezone")
+ const time = moment(moment())
+                    .format('HH:mm:ss')
+                moment.tz.setDefault(config.TIMEZONE)
+                    .locale('id')
+                const date = moment.tz(config.TIMEZONE).format('DD/MM/YYYY')
+
+                const totalMemoryBytes = os.totalmem();
+const freeMemoryBytes = os.freemem(); 
+
+    const day = Math.floor(uptime / (24 * 3600)); // Calculate days
+const hours = Math.floor((uptime % (24 * 3600)) / 3600); // Calculate hours
+const minutes = Math.floor((uptime % 3600) / 60); // Calculate minutes
+const seconds = Math.floor(uptime % 60); // Calculate seconds
+
+// Uptime
+const uptimeMessage = `*I am alive now since ${day}d ${hours}h ${minutes}m ${seconds}s*`;
+const runMessage = `*☀️ ${day} Day*\n*🕐 ${hours} Hour*\n*⏰ ${minutes} Minutes*\n*⏱️ ${seconds} Seconds*\n`;
+
+const xtime = moment.tz("Asia/Colombo").format("HH:mm:ss");
+const xdate = moment.tz("Asia/Colombo").format("DD/MM/YYYY");
+const time2 = moment().tz("Asia/Colombo").format("HH:mm:ss");
+let pushwish = "";
+
+if (time2 < "05:00:00") {
+  pushwish = `Good Morning 🌄`;
+} else if (time2 < "11:00:00") {
+  pushwish = `Good Morning 🌄`;
+} else if (time2 < "15:00:00") {
+  pushwish = `Good Afternoon 🌅`;
+} else if (time2 < "18:00:00") {
+  pushwish = `Good Evening 🌃`;
+} else if (time2 < "19:00:00") {
+  pushwish = `Good Evening 🌃`;
+} else {
+  pushwish = `Good Night 🌌`;
+}
+
         const settings = userSettings || global.CURRENT_BOT_SETTINGS || {};
         const botName = settings.botName || config.DEFAULT_BOT_NAME || "QUEEN-NILU-MD";
         const prefix = settings.prefix || config.DEFAULT_PREFIX || ".";
@@ -42,6 +80,9 @@ async (zanta, mek, m, { from, reply,q, userSettings }) => {
             .replace(/{BOT_NAME}/g, botName)
             .replace(/{OWNER_NUMBER}/g, config.OWNER_NUMBER)
             .replace(/{pushname}/g,user)
+            .replace(/{pushwish}/g,pushwish)
+            .replace(/{DATE}/g,date)
+            .replace(/{TIME}/g,time)
             .replace(/{PREFIX}/g, prefix);
 
         try {
--- plugins/aliveMsg.cjs
+++ plugins/aliveMsg.cjs
@@ -1,42 +1,5 @@
 function getAliveMessage() {
-    const config = require('../config.cjs');
-const moment = require("moment-timezone")
- const time = moment(moment())
-                    .format('HH:mm:ss')
-                moment.tz.setDefault(config.TIMEZONE)
-                    .locale('id')
-                const date = moment.tz(config.TIMEZONE).format('DD/MM/YYYY')
 
-                const totalMemoryBytes = os.totalmem();
-const freeMemoryBytes = os.freemem(); 
-
-    const day = Math.floor(uptime / (24 * 3600)); // Calculate days
-const hours = Math.floor((uptime % (24 * 3600)) / 3600); // Calculate hours
-const minutes = Math.floor((uptime % 3600) / 60); // Calculate minutes
-const seconds = Math.floor(uptime % 60); // Calculate seconds
-
-// Uptime
-const uptimeMessage = `*I am alive now since ${day}d ${hours}h ${minutes}m ${seconds}s*`;
-const runMessage = `*☀️ ${day} Day*\n*🕐 ${hours} Hour*\n*⏰ ${minutes} Minutes*\n*⏱️ ${seconds} Seconds*\n`;
-
-const xtime = moment.tz("Asia/Colombo").format("HH:mm:ss");
-const xdate = moment.tz("Asia/Colombo").format("DD/MM/YYYY");
-const time2 = moment().tz("Asia/Colombo").format("HH:mm:ss");
-let pushwish = "";
-
-if (time2 < "05:00:00") {
-  pushwish = `Good Morning 🌄`;
-} else if (time2 < "11:00:00") {
-  pushwish = `Good Morning 🌄`;
-} else if (time2 < "15:00:00") {
-  pushwish = `Good Afternoon 🌅`;
-} else if (time2 < "18:00:00") {
-  pushwish = `Good Evening 🌃`;
-} else if (time2 < "19:00:00") {
-  pushwish = `Good Evening 🌃`;
-} else {
-  pushwish = `Good Night 🌌`;
-}
 
     //const date = new Date().toLocaleDateString('en-US', { day: '2-digit', month: '2-digit', year: 'numeric' });
     //const time = new Date().toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false });
@@ -48,8 +11,8 @@ if (time2 < "05:00:00") {
 *╰────♡◉◉◉♡────⌬*
 
 
-*📅 ᴅᴀᴛᴇ: ${date}*
-*⌚ ᴛɪᴍᴇ: ${time}*
+*📅 ᴅᴀᴛᴇ: {DATE}*
+*⌚ ᴛɪᴍᴇ: {TIME}*
 *📱 ɴᴜᴍʙᴇʀ: {OWNER_NUMBER}*
 *💬 ᴘʀᴇꜰɪx: {PREFIX}*
 
