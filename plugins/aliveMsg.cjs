25ca3778f0a284b82ebee1a0d08d04e70298e93c
Janiya_X <95123183+Janithsadanuwan@users.noreply.github.com>
2026-03-20 18:49:17 +0530

    ᴊᴀɴɪʏᴀ

--- plugins/aliveMsg.cjs
+++ plugins/aliveMsg.cjs
@@ -1,6 +1,6 @@
 function getAliveMessage() {
 const moment = require("moment-timezone")
-    /* const time = moment(moment())
+ const time = moment(moment())
                     .format('HH:mm:ss')
                 moment.tz.setDefault(config.TIMEZONE)
                     .locale('id')
@@ -36,24 +36,24 @@ if (time2 < "05:00:00") {
 } else {
   pushwish = `Good Night 🌌`;
 }
-*/
-    const date = new Date().toLocaleDateString('en-US', { day: '2-digit', month: '2-digit', year: 'numeric' });
-    const time = new Date().toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false });
 
-    return `*{BOT_NAME} 𝐈𝐒 𝐎𝐍𝐋𝐈𝐍𝐄 💞*
+    //const date = new Date().toLocaleDateString('en-US', { day: '2-digit', month: '2-digit', year: 'numeric' });
+    //const time = new Date().toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false });
+
+    return `*👋🏻 {BOT_NAME} ONLINE NOW*
 
 *╭────♡◉◉◉♡────⌬*
-💖 *Hey{pushname}I’m {BOT_NAME}🙃, your lovely assistant — alive and sparkling now!!!!*
+💖 *Hey, {pushname} ${pushwish} I’m {BOT_NAME}🙃, Alive Now for you*
 *╰────♡◉◉◉♡────⌬*
+
 
 *📅 ᴅᴀᴛᴇ: ${date}*
 *⌚ ᴛɪᴍᴇ: ${time}*
-*───────────────*
-
 *📱 ɴᴜᴍʙᴇʀ: {OWNER_NUMBER}*
 *💬 ᴘʀᴇꜰɪx: {PREFIX}*
+
 *───────────────*
-*🌐 ᴄᴏɴᴛᴀᴄᴛ ᴢᴀɴᴛᴀ*
+*🌐 ᴄᴏɴᴛᴀᴄᴛ ᴊᴀɴɪʏᴀ*
 > http://wa.me/+94788175828?text=*Hey__JANIYA*
 `;
 }
