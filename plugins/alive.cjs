c73ff03e913c716050b92e654d8c2d2daa8e4d44
Janiya_X <95123183+Janithsadanuwan@users.noreply.github.com>
2026-03-20 20:08:45 +0530

    ds

--- plugins/alive.cjs
+++ plugins/alive.cjs
@@ -43,7 +43,8 @@ async (zanta, mek, m, { from, reply,q, userSettings }) => {
    
 
 const time2 = moment().tz("Asia/Colombo").format("HH:mm:ss");
-const pushwish = "";
+
+let pushwish = "";
 
 if (time2 < "05:00:00") {
   pushwish = `Good Morning 🌄`;
